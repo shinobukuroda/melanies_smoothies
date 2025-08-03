25728b3569d3ada13c750fbe52435734351edc47
skuroda <199155032+shinobukuroda@users.noreply.github.com>
2025-08-03 18:42:57 +1200

    Update streamit_app.py
    corrected a syntax.

--- streamit_app.py
+++ streamit_app.py
@@ -29,7 +29,7 @@ ingredients_list = st.multiselect(
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data.smoothiefroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 if ingredients_list:
     ingredients_string = ''
