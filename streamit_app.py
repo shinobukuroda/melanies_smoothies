742cd350a18b2f4143416ccc3eb1d7a35f6d940b
skuroda <199155032+shinobukuroda@users.noreply.github.com>
2025-08-04 17:16:42 +1200

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -42,7 +42,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen +' '
-   
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, 'is, 'search_on,'.'_
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders (ingredients, name_on_order)
