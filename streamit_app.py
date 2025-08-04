7a839f6f2c0c351b5129620f0d13ca3bcae2c4f5
skuroda <199155032+shinobukuroda@users.noreply.github.com>
2025-08-04 17:10:27 +1200

    Update streamit_app.py
    can search on 's' at the end of fruit names.

--- streamit_app.py
+++ streamit_app.py
@@ -16,8 +16,14 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 # session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'), col('SEARCH_ON'))
+#st.stop()
 #st.dataframe(data=my_dataframe, use_container_width=True)
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
